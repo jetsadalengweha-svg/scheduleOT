958bef0a40e500443dc41d2dec32fda79abdfeeb
jetsadalengweha-svg <jetsadalengweha@gmail.com>
2026-04-04 17:37:25 +0700

    Update y.py

--- y.py
+++ y.py
@@ -13,3 +13,18 @@ sheet_name = "ExpiredDrug"  # ชื่อชีทที่สอง
 url = 'https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}'.format(spreadsheet_id, sheet_name)
 datainput_cvs_format(url,'product.csv')
        
+def datainput_cvs_format(url,csvfile):
+       response = urllib.request.urlopen(url, context=context)
+       data = response.read().decode('utf-8')
+       rows = list(csv.reader(data.splitlines()))
+       header = rows[0]
+       data_rows = rows[1:]
+       f = open(csvfile, 'w' )
+       for row in data_rows:
+           name = row[1]
+           for i in range(4, len(header)):
+               label = header[i]
+               value = row[i]
+               if value.strip()  != "":          
+                   f.write ( '%s,%s,%s\n' % (label, name, value.lower()) )
+       f.close()
