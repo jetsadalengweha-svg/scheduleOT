373ffb68033d76e13a63bbc2e2e1e6d79f2cf82e
jetsadalengweha-svg <jetsadalengweha@gmail.com>
2026-04-04 17:34:39 +0700

    Update y.py

--- y.py
+++ y.py
@@ -10,3 +10,6 @@ spreadsheet_id = os.environ.get('SPREADSHEET_ID_PRODUCTEXPIRED')
 context = ssl._create_unverified_context()
 # เอา ID จาก URL ของ Google Sheet
 sheet_name = "ExpiredDrug"  # ชื่อชีทที่สอง
+url = 'https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}'.format(spreadsheet_id, sheet_name)
+datainput_cvs_format(url,'product.csv')
+       
